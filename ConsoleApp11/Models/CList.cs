bf1881f4b4bf03f28c80f10f26a83283c41b747b
muradmecidov <muradmecidov1499@gmail.com>
2023-04-27 11:19:00 +0400

    Task14

---
+++ ConsoleApp11/Models/CList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Models
+{
+    public class CList<T>
+    {
+        private List<T> list;
+
+        public CList() 
+        {
+            list = new List<T>();
+        }
+         public void Add(T item)
+        {
+            list.Add(item);
+        }
+        public void Clear()
+        {
+            list.Clear();
+        }
+        public int Count() 
+        { 
+            return list.Count; 
+        }
+        public bool Remove(T item)
+        {
+            list.Remove(item);
+            return true;
+        }
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        
+
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp11.Models;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CList<string> list = new CList<string>();
+            list.Add("MURAD");
+            list.Add("ALI");
+            list.Add("ELSAD");
+            list.Add("ORXAN");
+            list.Add("ILIKIN");
+
+
+            list.Remove("ILIKIN");
+            Console.WriteLine(list.Count());
+            Console.WriteLine(list.Contains("ALI"));
+           
+            //list.Remove("ILIKIN");
+            //list.Clear();
+
+        }
+    }
+}
